f2e053e299b39d8bd8ed834729ef85f6fe27f2be
omarr78 <omernabil2001@gmail.com>
2025-10-26 01:06:16 +0300

    implement add employee endpoint and all tests passed

--- src/main/java/com/internship/controller/EmployeeController.java
+++ src/main/java/com/internship/controller/EmployeeController.java
@@ -1,7 +1,24 @@
 package com.internship.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.internship.dto.CreateEmployeeRequest;
+import com.internship.entity.Employee;
+import com.internship.service.interfac.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/employees")
+@RequiredArgsConstructor
 public class EmployeeController {
+
+    private final EmployeeService service;
+
+    @PostMapping()
+    public ResponseEntity<Employee> CreateEmployee(
+            @RequestBody final CreateEmployeeRequest request) {
+        Employee employee = service.addEmployee(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(employee);
+    }
 }
