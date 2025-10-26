963ea49723639a146ce573ac90ba6ca5f33956db
omarr78 <omernabil2001@gmail.com>
2025-10-26 19:16:18 +0300

    add validation on date

--- src/main/java/com/internship/dto/CreateEmployeeRequest.java
+++ src/main/java/com/internship/dto/CreateEmployeeRequest.java
@@ -4,6 +4,7 @@ import com.internship.enums.Gender;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -19,9 +20,11 @@ public class CreateEmployeeRequest {
     private String name;
 
     @NotNull(message = "date of birth required")
+    @Past
     private LocalDate dateOfBirth;
 
     @NotNull(message = "graduation date required")
+    @Past
     private LocalDate graduationDate;
 
     @NotNull(message = "gender required")
