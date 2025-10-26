c2fb53567e4252c6baa9dd77611e63b9be32f2ec
omarr78 <omernabil2001@gmail.com>
2025-10-26 09:27:13 +0300

    Revert "implement add employee endpoint and all tests passed"
    This reverts commit f2e053e299b39d8bd8ed834729ef85f6fe27f2be.

--- src/main/java/com/internship/controller/EmployeeController.java
+++ src/main/java/com/internship/controller/EmployeeController.java
@@ -1,24 +1,7 @@
 package com.internship.controller;
 
-import com.internship.dto.CreateEmployeeRequest;
-import com.internship.entity.Employee;
-import com.internship.service.interfac.EmployeeService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/employees")
-@RequiredArgsConstructor
 public class EmployeeController {
-
-    private final EmployeeService service;
-
-    @PostMapping()
-    public ResponseEntity<Employee> CreateEmployee(
-            @RequestBody final CreateEmployeeRequest request) {
-        Employee employee = service.addEmployee(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(employee);
-    }
 }
--- src/main/java/com/internship/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/internship/service/impl/EmployeeServiceImpl.java
@@ -1,50 +1,14 @@
 package com.internship.service.impl;
 
 import com.internship.dto.CreateEmployeeRequest;
-import com.internship.entity.Department;
 import com.internship.entity.Employee;
-import com.internship.entity.Team;
-import com.internship.exception.ApiError;
-import com.internship.exception.BusinessException;
-import com.internship.mapper.EmployeeMapper;
-import com.internship.repository.DepartmentRepository;
-import com.internship.repository.EmployeeRepository;
-import com.internship.repository.TeamRepository;
 import com.internship.service.interfac.EmployeeService;
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import static com.internship.exception.ApiError.*;
-
 @Service
-@RequiredArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
-
-    private final EmployeeRepository employeeRepository;
-    private final DepartmentRepository departmentRepository;
-    private final TeamRepository teamRepository;
-    private final EmployeeMapper employeeMapper;
-
     @Override
-    @Transactional
     public Employee addEmployee(CreateEmployeeRequest request) {
-        Department department = departmentRepository.findById(request.getDepartmentId())
-                .orElseThrow(() -> new BusinessException(DEPARTMENT_NOT_FOUND,
-                        "Department not found with id: " + request.getDepartmentId()));
-
-        Team team = teamRepository.findById(request.getTeamId()).
-                orElseThrow(() -> new BusinessException(TEAM_NOT_FOUND,
-                        "Team not found with id: " + request.getTeamId()));
-
-        Employee manager = null;
-        if(request.getManagerId() != null){
-            manager = employeeRepository.findById(request.getManagerId()).
-                    orElseThrow(() -> new BusinessException(EMPLOYEE_NOT_FOUND,
-                            "Manager not found with id: " + request.getManagerId()));
-        }
-
-        Employee employee = employeeMapper.toEmployee(request,department,team,manager);
-        return employeeRepository.save(employee);
+        return null;
     }
 }
