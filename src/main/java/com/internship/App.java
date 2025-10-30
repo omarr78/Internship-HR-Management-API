d80d5d258b13f760f3c559d86eaa627910ff5296
omarr78 <omernabil2001@gmail.com>
2025-10-30 10:55:04 +0300

    set up project

--- src/main/java/com/internship/App.java
+++ src/main/java/com/internship/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HrManagementApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(HrManagementApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 
 }
