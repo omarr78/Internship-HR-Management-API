aa55ec8462212b1c9d2f30d442167040035abb88
omarr78 <omernabil2001@gmail.com>
2025-10-30 17:29:04 +0300

    edit on migration file in tests & edit on application.yml & remove unneccessary imports in EmployeeControllerTest

--- src/test/java/com/internship/integration/EmployeeControllerTest.java
+++ src/test/java/com/internship/integration/EmployeeControllerTest.java
@@ -7,17 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDate;
 
-import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
--- src/test/resources/db/testData/V1__create_employee_table.sql
+++ src/test/resources/db/testData/V1__create_employee_table.sql
@@ -6,8 +6,5 @@ CREATE TABLE employees
     graduation_date date                  NOT NULL,
     gender          VARCHAR(255)          NOT NULL,
     salary          FLOAT                 NOT NULL,
-    department_id   BIGINT                NOT NULL,
-    team_id         BIGINT                NOT NULL,
-    manager_id      BIGINT                NULL,
     CONSTRAINT pk_employees PRIMARY KEY (id)
 );
