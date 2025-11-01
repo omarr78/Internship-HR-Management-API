80608b9354ac270f15341f5010f174e3de2b449c
omarr78 <omernabil2001@gmail.com>
2025-11-01 14:47:20 +0200

    implement employee belongs to one team and tests passed

--- src/main/java/com/internship/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/internship/service/impl/EmployeeServiceImpl.java
@@ -4,15 +4,17 @@ import com.internship.dto.CreateEmployeeRequest;
 import com.internship.dto.EmployeeResponse;
 import com.internship.entity.Department;
 import com.internship.entity.Employee;
+import com.internship.entity.Team;
 import com.internship.exception.BusinessException;
 import com.internship.mapper.EmployeeMapper;
 import com.internship.repository.DepartmentRepository;
 import com.internship.repository.EmployeeRepository;
+import com.internship.repository.TeamRepository;
 import com.internship.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import static com.internship.exception.ApiError.DEPARTMENT_NOT_FOUND;
+import static com.internship.exception.ApiError.*;
 
 
 @Service
@@ -22,16 +24,24 @@ public class EmployeeServiceImpl implements EmployeeService {
     private final EmployeeRepository employeeRepository;
     private final DepartmentRepository departmentRepository;
     private final EmployeeMapper employeeMapper;
+    private final TeamRepository teamRepository;
 
     @Override
     public EmployeeResponse addEmployee(CreateEmployeeRequest request) {
+        if(!request.getDateOfBirth().isBefore(request.getGraduationDate())) {
+            throw new BusinessException(INVALID_EMPLOYEE_DATES_EXCEPTION);
+        }
+
         Department department = departmentRepository.findById(request.getDepartmentId())
                 .orElseThrow(() -> new BusinessException(DEPARTMENT_NOT_FOUND,
                         "Department not found with id: " + request.getDepartmentId()));
 
-        Employee employee = employeeMapper.toEmployee(request,department,null);
+        Team team = teamRepository.findById(request.getTeamId()).
+                orElseThrow(() -> new BusinessException(TEAM_NOT_FOUND,
+                        "Team not found with id: " + request.getTeamId()));
+
+        Employee employee = employeeMapper.toEmployee(request,department,team);
         Employee savedEmployee = employeeRepository.save(employee);
         return employeeMapper.toResponse(savedEmployee);
     }
-
 }
