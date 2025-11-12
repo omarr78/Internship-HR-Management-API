58a169f63eba4fe4ecd932b6d0c91408632e7251
omarr78 <omernabil2001@gmail.com>
2025-11-12 10:09:59 +0200

    write tests for modify employee & create updateRequest dto & create update_employee dataset

---
+++ src/main/java/com/internship/dto/UpdateEmployeeRequest.java
@@ -0,0 +1,29 @@
+package com.internship.dto;
+
+import com.internship.enums.Gender;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateEmployeeRequest {
+    @Size(min = 3, message = "name must not be empty and at least has 3 characters")
+    private String name;
+    @Past
+    private LocalDate dateOfBirth;
+    private LocalDate graduationDate;
+    private Gender gender;
+    @Min(0)
+    private float salary;
+    private Long departmentId;
+    private Long teamId;
+    private Optional<Long> managerId;
+    private List<String> expertises;
+}
