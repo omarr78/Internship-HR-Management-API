4bbf8d1a14fea18f85273bcf65d8e442f59b0d98
omarr78 <omernabil2001@gmail.com>
2025-11-25 10:15:14 +0200

    fix review to use conflict instead of bad request

--- src/main/java/com/internship/exception/ApiError.java
+++ src/main/java/com/internship/exception/ApiError.java
@@ -10,7 +10,7 @@ public enum ApiError {
     DEPARTMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "Department not found"),
     TEAM_NOT_FOUND(HttpStatus.NOT_FOUND, "Team not found"),
     INVALID_EMPLOYEE_DATES_EXCEPTION(HttpStatus.BAD_REQUEST, "graduation date must be after birth date at least 20 years"),
-    INVALID_EMPLOYEE_REMOVAL(HttpStatus.BAD_REQUEST, "Cannot remove manager (employee has subordinates) has no manager");
+    INVALID_EMPLOYEE_REMOVAL(HttpStatus.CONFLICT, "Cannot remove manager (employee has subordinates) has no manager");
 
     private final HttpStatus httpStatus;
     private final String defaultMessage;
