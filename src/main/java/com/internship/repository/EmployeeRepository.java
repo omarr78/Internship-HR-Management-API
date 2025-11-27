a2ce3773fb42ff9fea590103c2ff2631ac04a6ce
omarr78 <omernabil2001@gmail.com>
2025-11-27 12:56:56 +0200

    fix review avoid Test-Specific Queries and use repository function or sql query instead

--- src/main/java/com/internship/repository/EmployeeRepository.java
+++ src/main/java/com/internship/repository/EmployeeRepository.java
@@ -7,11 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    @Modifying(clearAutomatically = true, flushAutomatically = true)
-    @Query("UPDATE Employee e SET e.manager.id = :managerId WHERE e.id = :employeeId")
-    void updateManager(Long employeeId, Long managerId);
-
-    @Modifying(clearAutomatically = true, flushAutomatically = true)
+  @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query("UPDATE Employee AS e SET e.manager.id = :newManager WHERE e.manager.id = :oldManager")
     void reassignManager(@Param("oldManager") Long oldManager,
                          @Param("newManager") Long newManager);
