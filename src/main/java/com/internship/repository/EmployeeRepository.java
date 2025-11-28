89aaf119808d9393b59f1c91fa68886651f7ffe9
omarr78 <omernabil2001@gmail.com>
2025-11-28 15:23:06 +0200

    remove reduandant getSalary method

--- src/main/java/com/internship/repository/EmployeeRepository.java
+++ src/main/java/com/internship/repository/EmployeeRepository.java
@@ -7,12 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    @Query("SELECT e.salary FROM Employee e WHERE e.id = :id")
-    Optional<Float> getSalary(@Param("id") Long id);
-
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query("UPDATE Employee e SET e.manager.id = :managerId WHERE e.id = :employeeId")
     void updateManager(Long employeeId, Long managerId);
+
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query("UPDATE Employee AS e SET e.manager.id = :newManager WHERE e.manager.id = :oldManager")
     void reassignManager(@Param("oldManager") Long oldManager,
