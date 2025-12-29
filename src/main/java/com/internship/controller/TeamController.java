dce0076a82620eb49d2a6e4f0c68b93a529fa625
omarr78 <omernabil2001@gmail.com>
2025-12-29 15:28:51 +0200

    fix(review): fix review resource path should be single noun not plural

--- src/main/java/com/internship/controller/TeamController.java
+++ src/main/java/com/internship/controller/TeamController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/teams")
+@RequestMapping("/api/team")
 @RequiredArgsConstructor
 public class TeamController {
     private final TeamService service;
