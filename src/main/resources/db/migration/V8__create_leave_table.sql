58e044f7109b1b75b00b19fbac3e5480863c0e70
omarr78 <omernabil2001@gmail.com>
2026-01-14 16:46:31 +0200

    feat(db): add leave table in migration and the entity

---
+++ src/main/resources/db/migration/V8__create_leave_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE leaves
+(
+    id                 BIGINT AUTO_INCREMENT PRIMARY KEY,
+    leave_date         DATE    NOT NULL,
+    is_salary_deducted BOOLEAN NOT NULL,
+    employee_id        BIGINT  NOT NULL,
+
+    CONSTRAINT uq_leaves UNIQUE (employee_id, leave_date),
+    CONSTRAINT fk_employee_on_leaves
+        FOREIGN KEY (employee_id)
+            REFERENCES employees (id)
+)
