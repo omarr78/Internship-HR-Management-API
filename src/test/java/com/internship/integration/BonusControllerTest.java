26dc3c135e2648a802594b48ef55c3a0cf92f7d5
omarr78 <omernabil2001@gmail.com>
2026-01-20 09:49:52 +0200

    test: add test for adding bonus with no entered date in the request

---
+++ src/test/java/com/internship/integration/BonusControllerTest.java
@@ -0,0 +1,91 @@
+package com.internship.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.spring.api.DBRider;
+import com.internship.dto.CreateBonusRequest;
+import com.internship.dto.CreateBonusResponse;
+import com.internship.entity.Bonus;
+import com.internship.entity.Employee;
+import com.internship.repository.BonusRepository;
+import com.internship.repository.EmployeeRepository;
+import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.MediaType;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@DBRider
+public class BonusControllerTest {
+    static final Float POSITIVE_AMOUNT = 1000F;
+    static final Float NEGATIVE_AMOUNT = -500F;
+    static final Float ZERO_AMOUNT = 0F;
+    private static final Long NON_EXISTENT_ID = -1L;
+    private static final Long EXISTENT_EMPLOYEE_ID = 1L;
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private BonusRepository leaveRepository;
+    @Autowired
+    private BonusRepository bonusRepository;
+
+    @Test
+    @DataSet("dataset/create_bonus.xml")
+    public void testAddBonusWithoutEnteringDate_shouldSuccessAndReturnBonusDetailsWithDateOfToday() throws Exception {
+        // we will create bonus for employee with id 1 and its amount is 1000 and date not entered
+        CreateBonusRequest request = CreateBonusRequest.builder()
+                .amount(POSITIVE_AMOUNT)
+                .employeeId(EXISTENT_EMPLOYEE_ID)
+                .build();
+
+        final LocalDate mockedToday = LocalDate.of(2020, 1, 1);
+        try (MockedStatic<LocalDate> mocked = Mockito.mockStatic(LocalDate.class, Mockito.CALLS_REAL_METHODS)) {
+            mocked.when(LocalDate::now).thenReturn(mockedToday);
+            MvcResult result = mockMvc.perform(post("/api/bonus")
+                            .contentType(String.valueOf(MediaType.APPLICATION_JSON))
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andExpect(status().isCreated())
+                    .andReturn();
+
+            CreateBonusResponse response = objectMapper.readValue(result.getResponse().getContentAsString(),
+                    CreateBonusResponse.class);
+
+            CreateBonusResponse expectedBonusResponse = new CreateBonusResponse(mockedToday, EXISTENT_EMPLOYEE_ID);
+
+            // assertion on response
+            Assertions.assertThat(response)
+                    .usingRecursiveComparison()
+                    .ignoringFields("id")
+                    .isEqualTo(expectedBonusResponse);
+
+            Employee employee = employeeRepository.findById(EXISTENT_EMPLOYEE_ID).get();
+
+            Bonus expectedBonus = new Bonus(mockedToday, POSITIVE_AMOUNT, employee);
+            List<Bonus> bonuses = bonusRepository.findAll();
+
+            // assertion on database
+            Assertions.assertThat(bonuses)
+                    .usingRecursiveFieldByFieldElementComparatorIgnoringFields("id")
+                    .contains(expectedBonus);
+        }
+    }
+}
