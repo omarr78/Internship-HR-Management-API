c310c6de57197935cb348a9c5491ea441c65e818
omarr78 <omernabil2001@gmail.com>
2026-02-04 13:48:29 +0200

    refactor: make the creationDate filled automatically in jpa by enabling auditing and in database

--- src/main/java/com/internship/entity/EmployeeSalary.java
+++ src/main/java/com/internship/entity/EmployeeSalary.java
@@ -3,6 +3,8 @@ package com.internship.entity;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -13,23 +15,25 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Builder
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "EMPLOYEE_SALARIES")
 public class EmployeeSalary {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "DATE", nullable = false)
-    private LocalDate date;
+    @Column(name = "CREATION_DATE", nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDate creationDate;
 
-    @Column(name = "GROSS_SALARY", nullable = false)
+    @Column(name = "GROSS_SALARY", nullable = false, updatable = false)
     private BigDecimal grossSalary;
 
-    @Column(name = "REASON", nullable = false)
+    @Column(name = "REASON", nullable = false, updatable = false)
     private String reason;
 
     @ManyToOne
-    @JoinColumn(name = "EMPLOYEE_ID", nullable = false)
+    @JoinColumn(name = "EMPLOYEE_ID", nullable = false, updatable = false)
     @JsonBackReference("employee-salary")
     private Employee employee;
 }
--- src/main/resources/db/migration/V11__create_salary_table.sql
+++ src/main/resources/db/migration/V11__create_salary_table.sql
@@ -1,9 +1,10 @@
 CREATE TABLE employee_salaries
 (
-    id           BIGINT AUTO_INCREMENT NOT NULL,
-    date         date                  NOT NULL,
-    gross_salary DECIMAL(15, 2)        NOT NULL,
-    reason       VARCHAR(255)          NOT NULL,
-    employee_id  BIGINT                NOT NULL,
-    CONSTRAINT pk_employee_salaries PRIMARY KEY (id)
+    id            BIGINT AUTO_INCREMENT NOT NULL,
+    creation_date DATE                  NOT NULL DEFAULT (CURRENT_DATE),
+    gross_salary  DECIMAL(15, 2)        NOT NULL,
+    reason        VARCHAR(255)          NOT NULL,
+    employee_id   BIGINT                NOT NULL,
+    CONSTRAINT pk_employee_salaries PRIMARY KEY (id),
+    CONSTRAINT fk_employee_on_employee_salaries FOREIGN KEY (employee_id) REFERENCES employees (id)
 );
