17d558da107a4da9909d2afed4b251205ab0de72
omarr78 <omernabil2001@gmail.com>
2026-02-12 11:50:32 +0200

    feat: implement happy scenario of raising employee salary

--- src/main/java/com/internship/controller/EmployeeController.java
+++ src/main/java/com/internship/controller/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.internship.controller;
 
 import com.internship.dto.*;
+import com.internship.entity.Employee;
+import com.internship.entity.EmployeeSalary;
+import com.internship.mapper.EmployeeSalaryMapper;
 import com.internship.repository.EmployeeRepository;
 import com.internship.repository.EmployeeSalaryRepository;
 import com.internship.service.EmployeeService;
@@ -10,7 +13,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
+
+import static com.internship.enums.SalaryReason.SALARY_RAISED;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -19,6 +25,7 @@ public class EmployeeController {
     private final EmployeeService service;
     private final EmployeeRepository employeeRepository;
     private final EmployeeSalaryRepository employeeSalaryRepository;
+    private final EmployeeSalaryMapper employeeSalaryMapper;
 
     @PostMapping
     public ResponseEntity<EmployeeResponse> createEmployee(@RequestBody @Valid CreateEmployeeRequest request) {
@@ -37,6 +44,25 @@ public class EmployeeController {
     public ResponseEntity<SalaryResponse> updateSalary(@RequestBody @Valid final UpdateSalaryRequest request,
                                                        @PathVariable final Long id) {
         SalaryResponse response = service.modifySalary(request, id);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PostMapping("{id}/salary-raises")
+    public ResponseEntity<SalaryResponse> raiseSalary(@RequestBody @Valid final RaiseSalaryRequest request,
+                                                      @PathVariable final Long id) {
+        Employee employee = employeeRepository.findById(id).get();
+        BigDecimal currentGrossSalary = employee.getGrossSalary();
+
+        // insert employee salary in employee-salaries table
+        EmployeeSalary employeeSalary = EmployeeSalary.builder()
+                .grossSalary(currentGrossSalary.add(request.getAmount()))
+                .reason(SALARY_RAISED.getMessage())
+                .employee(employee)
+                .build();
+
+        EmployeeSalary savedEmployeeSalary = employeeSalaryRepository.save(employeeSalary);
+
+        SalaryResponse response = employeeSalaryMapper.toResponse(savedEmployeeSalary);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
